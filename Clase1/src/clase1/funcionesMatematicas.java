264422c9df3034fd3b1efcbb5845f6f463618c81
Melvin Borja <rb0587032020@unab.edu.sv>
2020-08-11 16:42:56 -0600

    PRACTICA
    PRACTICA DE LA CLASE

---
+++ Clase1/src/clase1/funcionesMatematicas.java
@@ -0,0 +1,42 @@
+
+package clase1;
+
+
+public class funcionesMatematicas {
+   
+    int Suma (){
+        int dato1= 12; 
+        int dato2= 10;
+        int resultado;
+        
+        resultado = dato1 + dato2;
+       
+        
+        return resultado;
+    }
+    
+        int Resta() {
+        int dato1= 12; 
+        int dato2= 10;
+        int resultado;
+        
+        resultado = dato1 - dato2;
+        
+        
+        return resultado;
+    }
+        
+        double Divicion () {
+        int dato1= 12; 
+        int dato2= 10;
+        int resultado;
+        
+        resultado = dato1 / dato2;
+        
+        return resultado;
+        
+        }             
+    
+}
+
+
